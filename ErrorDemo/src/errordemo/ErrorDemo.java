564cb3b4ebb14b84467c68a14206ce04a69803e9
yoges <yoges@Yogesh-SP4>
2017-07-11 08:44:41 -0400

    This is a basic Java tutorial on handling exception errors.

---
+++ ErrorDemo/src/errordemo/ErrorDemo.java
@@ -0,0 +1,35 @@
+
+package errordemo;
+
+import java.util.Scanner;
+
+public class ErrorDemo {
+
+  //This short programs show's how to execute exception errors.
+    
+    public static void main(String[] args) {
+        int num, deno;
+        
+        Scanner input = new Scanner(System.in);
+        
+        try
+        {
+            System.out.print("Please enter the numerator: ");
+            num = input.nextInt();
+            
+            System.out.print("Please enter the denominator: ");
+            deno = input.nextInt();
+            
+            System.out.println("The result is " + num/deno);
+        }
+        catch (Exception e)
+                {
+                    System.out.println(e.getMessage());
+                }
+        finally
+                {
+                    System.out.println("---- End of Error Handling Example ----");
+                }
+    }
+    
+}
